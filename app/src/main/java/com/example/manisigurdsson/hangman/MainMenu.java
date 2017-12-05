3b9c1037d9509d15f1f3c5565a56b1d7470d8784
Máni Sigurðsson <manisigurdsson@Manis-MacBook-Pro-2.local>
2017-12-05 13:23:01 +0000

    bug fix fyrir facebook login

--- app/src/main/java/com/example/manisigurdsson/hangman/MainMenu.java
+++ app/src/main/java/com/example/manisigurdsson/hangman/MainMenu.java
@@ -1,9 +1,10 @@
 package com.example.manisigurdsson.hangman;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -11,20 +12,28 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+
 public class MainMenu extends AppCompatActivity {
+    public static final int REQ_ID = 1111;
     LoginButton loginButton;
     CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        if(isLoggedIn()){
+            Intent intent = new Intent(MainMenu.this, Menu.class);
+            startActivityForResult(intent, REQ_ID);
+        }
+        else{
         setContentView(R.layout.activity_main_menu);
         loginButton = findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                Intent intent = new Intent(MainMenu.this, Menu.class);
+                startActivityForResult(intent, REQ_ID);
             }
 
             @Override
@@ -36,10 +45,15 @@ public class MainMenu extends AppCompatActivity {
             public void onError(FacebookException error) {
 
             }
-        });
+        });}
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
     }
 }
