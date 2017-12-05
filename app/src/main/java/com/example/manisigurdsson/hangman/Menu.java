5710ae125ed16ad7d2af79db2130220c215eecfd
Máni Sigurðsson <manisigurdsson@Manis-MacBook-Pro-2.local>
2017-12-05 13:36:03 +0000

    menu Branch ready fyrir merge

--- app/src/main/java/com/example/manisigurdsson/hangman/Menu.java
+++ app/src/main/java/com/example/manisigurdsson/hangman/Menu.java
@@ -2,6 +2,7 @@ package com.example.manisigurdsson.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void ClickedTwoPlayer(View view) {
+        // TODO
+    }
+
+    public void ClickedOptions(View view) {
+        // TODO
+    }
+
+    public void ClickedSinglePlayer(View view) {
+        // TODO
     }
 }
